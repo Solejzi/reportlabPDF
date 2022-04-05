13e2588ad3f81bf747d4f2285481a3d0db12ca5a
Mikołaj Nyga <mikolajnyga1@gmail.com>
2022-04-05 15:45:52 +0200

    Update content.py

--- content.py
+++ content.py
@@ -1,8 +1,8 @@
 class Content():
     def __init__(self):
         pass
-    file_name = 'folderLex1.pdf'
-    page_info = 'Lex Secure Pdf'
+    file_name = '1.pdf'
+    page_info = 'L Pdf'
     doc_title = 'Doc TiTle @!@'
 
     page_size = (1488, 2104)
@@ -20,8 +20,8 @@ class Content():
     frontpage_img = 'frontpage.png'
 
     #p1
-    frontpage_edit = "PRAWNIK DLA"
-    frontpage_whom = 'MAMY I TATY'
+    frontpage_edit = "PR"
+    frontpage_whom = 'MY'
 
     #2page
     p2_title = 'I.  INFORMACJE OGÓLNE'
@@ -31,113 +31,89 @@ class Content():
         'firma łos s.a',
         'ul. jakaś',
         '00-000 biegun']
-    p2_service = 'w zakresie świadczenia usługi Lex Secure 24h Opieka Prawna w ramach programu Prawnik dla'
+    p2_service = 'w zadla'
 
 
-    p2_intro = ['Szanowni Państwo,',
+    p2_intro = ['Szastwo,',
     'Niniejszym przedstawiamy Państwu Ofertę w zakresie świadczenia usług',
-    'Abonamentów Prawnych przez Lex Secure 24h Opieka Prawna',
-    'Komar-Komarowski Spółka komandytowa z siedzibą w Sopocie (dalej zwana jako Lex Secure) dla']
+    'Abon',
+    'K']
     p2_name = frontpage_whom
 
     #p3
 
-    p3_header = 'Abonamenty Prawne Lex Secure to:'
+    p3_header = 'Aboto:'
     p3_bullets = [
-        'całodobowa Infolinia Prawna z dedykowanym dla Państwa numerem telefonu,',
-        'nieograniczona ilość zapytań prawnych,',
-        'doświadczony, wieloosobowy zespół prawników (radców prawnych, adwokatów),',
-        'bardzo szeroki zakres prawny,',
-        'gotowe szablony i wzory dokumentów prawnych,',
-        'dostęp do aktualnych przepisów prawnych',
-        'rozwiązanie problemu prawnego w 24h, zarówno w sprawach zawodowych, jak i sprawach prywatnych,',
-        'wszystkie odpowiedzi na pytania prawne sygnowane są przez radców prawnych i adwokatów,',
+        'całodobowa Inf,',
+     
         'wideoporady prawne.']
 
     #p4
-    p4_title = 'II. CZĘŚĆ MERYTORYCZNA OFERTY'
-    p4_subtitle = '1. Opis działalności Lex Secure'
-    p4_text = '''Spółka Lex Secure, której usługi mamy zaszczyt Państwu zaoferować, posiada swoją siedzibę w Sopocie przy al. Niepodległości 723/6. Jest to Spółka założona w roku 1989 w Gdyni – pierwotnie zarejestrowana, jako spółka z ograniczoną odpowiedzialnością, obecnie Spółka zarejestrowana jest w Sądzie Rejonowym Gdańsk-Północ w Gdańsku VII Wydział Gospodarczy pod numerem KRS 00000675528. Spółka posiada NIP nr 5862017484 oraz REGON 191884226. Kapitał zakładowy Spółki wynosi 210.100,00 złotych i został pokryty w całości gotówką.
-W roku 2011 Lex Secure zmieniła swój dotychczasowy profil działalności z działalności typowo doradczej na działalność tzw. Operatora Prawnego, którego główną działalnością obecnie jest świadczenie usług opieki prawnej w ramach abonamentu Lex Secure 24h Opieka Prawna poprzez 24h Infolinię prawną oraz internetową Strefę Klienta.
-Lex Secure stworzyła i stale rozwija Ogólnopolską Sieć Opieki Prawnej Lex Secure, w skład której wchodzą renomowane kancelarie adwokackie oraz radcowskie, które współpracują z Lex Secure, jako rekomendowani prawnicy.
-Zaznaczyć należy, że Lex Secure na przełomie kwietnia/maja 2017 roku przekształciła się w formę spółki komandytowej i została zgłoszona do Okręgowej Izby Radców Prawnych w Gdańsku, jako kancelaria radców prawnych i adwokatów – tym samym wszystkie usługi prawne świadczone przez naszą Spółkę objęte są ubezpieczeniem OC radców prawnych i adwokatów. Takiego ubezpieczenia nie posiadają inni operatorzy prawni działający w formie spółek kapitałowych.'''
+    p4_title = 'II. CZĘŚĆ MERY'
+    p4_subtitle = '1. Opis działre'
+    p4_text = '''Sałek kapitałowych.'''
     #p5
-    p5_subtitle = '2. Opis usługi Lex Secure'
-    p5_text = '''Opieka prawna, którą prowadzi Lex Secure dla Klientów masowych w ramach abonamentu Lex Secure 24h Opieka Prawna, polega na udzielaniu odpowiedzi na pytania prawne Klientów kierowane do naszych prawników za pośrednictwem 24h Infolinii prawnej, która została uruchomiona na bazie profesjonalnych rozwiązań teleinformatycznych. Rozwój 24-godzinnej Infolinii prawnej spowodował konieczność przygotowania specjalnego dedykowanego oprogramowania, które służy do obsługi Infolinii, obsługi Klientów oraz kancelarii współpracujących, ponadto oprogramowanie to monitoruje skuteczność i efektywność wszystkich procesów, tak aby finalnie odpowiedź na postawione nam pytanie prawne zostałaprzesłana do Klienta końcowego w terminie nie dłuższym niż 24 godziny od czasu zakończenia rozmowy telefonicznej.
-Abonament Lex Secure 24h Opieka Prawna to unikatowy program umożliwiający uzyskanie odpowiedzi na pytanie prawne w formie dokumentu przesyłanego drogą elektroniczną, sygnowanego zawsze przez radcę prawnego lub adwokata. Do dyspozycji naszych Klientów, objętych abonamentem, oddajemy całodobową infolinię, na której każdego dnia przez 24-godziny na dobę pełni dyżur zespół doświadczonych prawników, przyjmujący pytania prawne. Jeżeli sprawa Klienta wymaga postępowania sądowego, proponujemy możliwość skorzystania z obsługi na preferencyjnych warunkach przez sieć współpracujących z nami kancelarii prawnych na terenie całego kraju.
-Dodatkowo każdy Klient korzystający z naszego abonamentu posiada dostęp za pośrednictwem 24h Infolinii prawnej do Bazy Wzorów Dokumentów Lex Secure, która to baza obejmuje wzorce umów oraz standardowe pisma w postępowaniu cywilnym, administracyjnym oraz karnym. Jednocześnie każdy Klient ma dostęp do wszystkich obowiązujących w Polsce aktów prawnych, które na żądanie Klienta wysyłane są na wskazany przez Klienta adres e-mail.
-Cały proces opieki prawnej odbywa się wieloetapowo, przy czym nasi radcy prawni i adwokaci są dodatkowo weryfikowani przez bardzo doświadczonych kolegów.
-Obecnie nasz zespół radców prawnych i adwokatów bezpośrednio współpracujących z Lex Secure 24h Opieka Prawna to około 150 profesjonalnych prawników. Łączna liczba prawników (prawników bez aplikacji, aplikantów radcowskich i adwokackich, radców prawnych i adwokatów) zaangażowanych w codzienną pracę Lex Secure to 43 osoby – radcy prawni, adwokaci oraz doradcy podatkowi.
-Dodatkowo Lex Secure 24h Opieka Prawna korzysta z sieci Podwykonawców, których potencjał osobowy to dodatkowo kilkudziesięciu profesjonalnych prawników.'''
+    p5_subtitle = '2. Opiecure'
+    p5_text = '''Opiów.'''
     #p6
-    p6_subtitle = '3. Doświadczenie Lex Secure'
-    p6_text = '''Lex Secure 24h Opieka Prawna od ponad 9 lat z sukcesem świadczy usługę 24h Opieki Prawnej dla ponad 152.000 Klientów: Członków Związków Zawodowych w Polsce, Członków Izb Zawodowych w Polsce (w szczególności Okręgowych Izb Lekarskich oraz Okręgowych Izb Aptekarskich), Członków Stowarzyszeń Zawodowych, Pracowników Firm w Polsce, dla sektora finansowego i ubezpieczeniowego (od maja 2017 r. usługa świadczona jest dla Towarzystwa Ubezpieczeń INTER Polska S.A.) oraz Fundacji.'''
+    p6_subtitle = '3. Dośure'
+    p6_text = '''Leji.'''
     #p7
-    p7_subtitle = '4. Zespół Lex Secure'
-    p7_text = '''Osobami zarządzającymi całym procesem opieki prawnej w ramach abonamentu Lex Secure 24h Opieka Prawna są następujący adwokaci i radcy prawni:'''
+    p7_subtitle = '4. Zespe'
+    p7_text = '''Osni:'''
     p7_list = [
-        '''Partner Zarządzający radca prawny Kajetan Komar-Komarowski (nr wpisu GD/GD/1579) – specjalista w zakresie prawa gospodarczego, prawa spółek i instytucji publicznych, w tym prawa medycznego, nieruchomości, pracy, ubezpieczeniowego, bankowego,''',
-        '''Partner Zarządzający radca prawny Magdalena Komar–Komarowska (nr wpisu GD/GD/1578) – specjalistka w zakresie prawa medycznego, farmaceutycznego, prawa bankowego i spółdzielczego, prawa pracy oraz postępowania cywilnego i administracyjnego,''',
-        '''Partner Zarządzający radca prawny Andrzej Sulima (nr wpisu GD/GD/1590) – specjalista z zakresu obsługi podmiotów gospodarczych, prawa handlowego, podatkowego, prawa pracy i prawa autorskiego. Posiada bogate doświadczenie na gruncie prawa postępowania cywilnego i administracyjnego,''',
-        '''Partner Zarządzający radca prawny Dorota Piotrowska (nr wpisu GD/GD/1574) – specjalizująca się w prawie rodzinnym, prawie cywilnym, procedurze cywilnej, prawie upadłościowym i naprawczym,''',
-        '''Partner Zarządzający radca prawny Krzysztof Biela (nr wpisu GD/GD/1633) – specjalizujący się w prawie nieruchomości i w obsłudze wspólnot mieszkaniowych, spółdzielni mieszkaniowych. Prowadzi liczne postępowania sądowe na terenie całej Polski.'''
+        '''zi liczne poej Polski.'''
     ]
     #p8
 
-    p8_subtitle = '5.2. Pakiety Prawne'
-    p8_text = 'Lex Secure na bazie swoich ugruntowanych doświadczeń, proponuje dla'+ frontpage_whom+ ' dwa pakiety opieki prawnej – Pakiet Standard oraz Pakiet Premium, (zgodnie z Regulaminem Świadczenia Usług Serwisu „Lex Secure 24h Opieka Prawna” zamieszczony na stronie internetowej www.lexsecure.pl).'
-    p8_sub1 = '5.2.1. Pakiet Standard'
-    p8_text1 = 'Pakiet Standard obejmuje swoim zakresem:'
-    p8_list1 = ['Pakiet Standard obejmuje swoim zakresem całe prawo polskie oraz prawo europejskie, obowiązujące na terenie Rzeczpospolitej Polskiej.',
-                'Pakiet Standard obejmuje następujące usługi Pomocy prawnej:',
-                '3. Pakiet Standard obejmuje swoim zakresem Klienta.']
-    p8_bullet1 = ['1) 24h Infolinia Prawna,',
-                '2) Porady prawne,']
+    p8_subtitle = '5.2. Pane'
+    p8_text = 'L'+ frontpage_whom+ ' .'
+    p8_sub1 = '5ard'
+    p8_text1 = 'Paesem:'
+    p8_list1 = ['iej.',
+                'Pakwnej:',
+                '3. Pa.']
+    p8_bullet1 = ['1) 2na,',
+                '2)e,']
 
-    p8_sub2 = '5.2.2. Pakiet Premium'
-    p8_list2 = ['Pakiet Standard obejmuje swoim zakresem całe prawo polskie oraz prawo europejskie, obowiązujące na terenie Rzeczpospolitej Polskiej.',
-                'Pakiet Standard obejmuje następujące usługi Pomocy prawnej:',
-                '3. Pakiet Standard obejmuje swoim zakresem Klienta oraz Osoby bliskie.']
+    p8_sub2 = '5.um'
+    p8_list2 = ['P.',
+                'Pj:',
+                '3..']
     p8_bullet2 = [
-        '1) 24h Infolinia Prawna,',
-        '2) Porady prawne,',
-        'Dokumenty prawne,',
-        'Wideoporady prawne,',
-        'Wybór Adwokata do sprawy sądowej.']
-    p8_bottext = 'Uprawniony uzyskuje wymienione powyżej świadczenia 24h na dobę 7 dni w tygodniu, bez ograniczenia ilości zgłoszeń, przy czym kolejne zgłoszenie jest realizowane po zakończeniu zgłoszenia wcześniejszego. w przypadku skomplikowanych opinii prawnych lub pytań prawnych, ustalamy termin realizacji usługi indywidualnie z naszym Klientem.'
+        '1a,',
+        '2e,',
+        'Done,',
+        'Widne,',
+        'Wyej.']
+    p8_bottext = 'Upntem.'
 
     #p9
-    p9_subtitle = '5.2. Pakiety Prawne'
-    p9_text = '''Jednym z najważniejszych elementów masowej opieki prawnej jest system identyfikacji stosowany przez Operatora Prawnego. Ma to istotne znaczenie z uwagi na restrykcyjne przepisy dotyczące ochrony danych osobowych oraz konieczność zachowania tajemnicy adwokackiej i radcy prawnego.' \
-              Lex Secure proponuje rozwiązanie najprostsze z punktu widzenia wymogów prawa. Jedynymi danymi, jakie są niezbędne dla rozpoczęcia świadczeniu usługi prawnej to dwa niepowtarzalne numery identyfikacyjne nadane Klientowi oraz rodzaj wariantu usługi. Zaznaczyć należy, że w/w dane nie są danymi osobowymi. Na podstawie tych danych system Lex Secure jest w stanie zweryfikować osobę korzystającą z usługi. Dopiero przy pierwszym kontakcie Klienta poprzez 24h Infolinię Prawną lub Strefę Klienta, Lex Secure pobiera wszystkie wymagane prawem zgody na przetwarzanie danych osobowych oraz świadczenie usług drogą elektroniczną. Każda rozmowa lub kontakt telefoniczny z Lex Secure jest archiwizowany przez okres co najmniej 6 lat (okres przedawnienia roszczeń).
-Powyższe rozwiązanie polega na komunikacji pomiędzy systemem informatycznym Lex Secure oraz systemem np. Państwa firmy. w żadnym przypadku system Lex Secure nie ma dostępu do systemu zewnętrznego oraz odwrotnie. Lex Secure posiada narzędzia informatyczne służące do komunikacji pomiędzy różnymi systemami.
-Lex Secure dodatkowo proponuje wyprodukowanie i dystrybucję kart plastikowych dla każdego Klienta, według ustalonego wzoru. Koszt produkcji oraz dystrybucji kart ponosi Zamawiający. Wskazujemy, że wraz z dystrybucją kart można przeprowadzić akcję informacyjną (koszt ulotek pokryje Lex Secure), która należałoby przeprowadzić w sposób wskazany przez Zamawiającego. Akcja informacyjna ma na celu wyjaśnienie Klientom jak korzystać z usługi.'''
+    p9_subtitle = '5.2. rawne'
+    p9_text = '''Jei.'''
     #p10
-    p10_subtitle1 = '7. Ochrona tajemnicy adwokackiej lub radcy prawnego'
-    p10_text1 = 'Lex Secure udostępnia akta danej sprawy wyłącznie Klientowi, którego sprawa dotyczy. Nie istnieje możliwość udostępnienia danych z konkretnej sprawy na rzecz podmiotów trzecich z uwagi na konieczność zachowania tajemnicy adwokackiej i/lub radcy prawnego. Wyłącznie takie rozwiązanie zapewnia poufność w relacji prawnik – Klient.'
-    p10_subtitle2 = '8. Osoby upoważnione do negocjacji oferty'
-    p10_list = ['''a. Kajetan Komar-Komarowski – Wspólnik Zarządzający
-Tel. kom. +48 600 017 471
-e-mail: kajetan.komar-komarowski@opiekaprawna.pl''',
-                '''Paweł Żółtowski – Dyrektor ds. Sprzedaży
-Tel. kom. +48 601 141 656
-e-mail: pawel.zoltowski@opiekaprawna.pl''']
+    p10_subtitle1 = 'ego'
+    p10_text1 = 'Leilient.'
+    p10_subtitle2 = '8.ferty'
+    p10_list = ['''aający
+Tel. kl''',
+                '''Pal''']
     #p11
-    p11_title = 'III. CZĘŚĆ FINANSOWA OFERTY'
-    p11_subtitle = '1. Oferta cenowa'
-    p11_text = 'Lex Secure, proponuje następujące stawki cenowe dla przedstawionych w niniejszej Ofercie abonamentów prawnych.'
-    p11_sub1 = '1.1. Pakiet Standard'
-    p11_txt1 = f'Oferowana cena jednostkowa z tytułu opieki prawnej w zakresie PAKIETU STANDARD dla {frontpage_whom}. wynosi: {price} zł brutto (z VAT) miesięcznie.'
-    p11_sub2 = '1.2. Pakiet Premium'
-    p11_txt2 = f'Oferowana cena jednostkowa z tytułu opieki prawnej w zakresie PAKIETU PREMIUM dla {frontpage_whom}. wynosi: {price} zł brutto (z VAT) miesięcznie.'
-    p11_sub3 = '2. Okres ważności oferty'
-    p11_txt3 = f'Niniejsza Oferta jest ważna do dnia {date} roku.'
-    p11_bottext = '''Warunki finansowe naszej oferty oraz zakres merytoryczny usługi jesteśmy gotowi omówić podczas spotkania z Państwa zespołem negocjacyjnym.
-Z poważaniem'''
+    p11_title = 'III.ERTY'
+    p11_subtitle = '1wa'
+    p11_text = 'ych.'
+    p11_sub1 = '1ard'
+    p11_txt1 = f'Oferowana a {frontpage_whom}. w: {price} zł brutto (z VAT) miesięcznie.'
+    p11_sub2 = '1.m'
+    p11_txt2 = f'Ofa {frontpage_whom}. w {price} zł bruiesięcznie.'
+    p11_sub3 = '2. Okty'
+    p11_txt3 = f'Ni {date} roku.'
+    p11_bottext = '''Warunki fić podccyjnym.
+m'''
     #p12
     p12_title = 'IV. ZAŁĄCZNIKI DO OFERTY'
-    p12_list = ['1. Regulamin Świadczenia Usług Serwisu „Lex Secure 24h Opieka Prawna”']
-    p12_text = '''Klauzula Prawna:
-Wszystkie dokumenty, wzory Regulaminów, projekty umów - załączone do niniejszej Oferty, stanowią przejaw oryginalnej, indywidualnej twórczości Lex Secure, jako utwory w rozumieniu ustawy o prawie autorskim i prawach pokrewnych. Ich kopiowanie, modyfikacja, przetwarzanie lub udostępnianie może nastąpić wyłącznie za zgodą Lex Secure i podlega ochronie prawnej.
+    p12_list = ['1. Regna”']
+    p12_text = '''Klaawna:
+ej.
 '''
 cont_obj = Content()
